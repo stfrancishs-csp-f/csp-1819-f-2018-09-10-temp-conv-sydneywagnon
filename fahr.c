86de091e100d3d500cdfe599c4a655033ba9ad2a
Sydney Wagnon <20wagnon@stfrancishs.org>
2018-09-11 18:09:28 +0000

    change initial value

--- fahr.c
+++ fahr.c
@@ -4,7 +4,8 @@ int main(void)
 {
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr +20)
+    for (fahr = -100
+    ; fahr <= 300; fahr = fahr +20)
     {
         printf ("f - %3d c - %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
